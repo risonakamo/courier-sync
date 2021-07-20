398212f3cc474f357faaa4e55d8a511f75396080
risonakamo <hmykanae@gmail.com>
2021-07-20 12:36:05 -0400

    split off mirror diff row

---
+++ web/components/MirrorDiffTable/MirrorDiffRow/MirrorDiffRow.svelte
@@ -0,0 +1,22 @@
+<div class="row item">
+  <div class="col status-col"></div>
+  <div class="col name-col">
+    <div class="slot icon-slot folder-arrow">
+      <img src="/assets/temp_folderarrow2.png" alt="folder-arrow"/>
+    </div>
+    <div class="slot icon-slot">
+      <img src="/assets/temp_folder.png" alt="folder"/>
+    </div>
+    <div class="slot name-slot">
+      folder
+    </div>
+  </div>
+  <div class="col size-col">
+    <div class="slot divide-slot">200.7 kb</div>
+    <div class="slot divide-slot"></div>
+  </div>
+  <div class="col modified-col">
+    <div class="slot divide-slot">2021/07/15 20:30</div>
+    <div class="slot divide-slot"></div>
+  </div>
+</div>
--- web/components/MirrorDiffTable/MirrorDiffTable.svelte
+++ web/components/MirrorDiffTable/MirrorDiffTable.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import MirrorDiffRow from "./MirrorDiffRow/MirrorDiffRow.svelte";
+
   import "./MirrorDiffTable.less";
 </script>
 
@@ -13,26 +15,5 @@
     <div class="col modified-col">Modified</div>
   </div>
 
-  <div class="row item">
-    <div class="col status-col"></div>
-    <div class="col name-col">
-      <div class="slot icon-slot folder-arrow">
-        <img src="/assets/temp_folderarrow2.png" alt="folder-arrow"/>
-      </div>
-      <div class="slot icon-slot">
-        <img src="/assets/temp_folder.png" alt="folder"/>
-      </div>
-      <div class="slot name-slot">
-        folder
-      </div>
-    </div>
-    <div class="col size-col">
-      <div class="slot divide-slot">200.7 kb</div>
-      <div class="slot divide-slot"></div>
-    </div>
-    <div class="col modified-col">
-      <div class="slot divide-slot">2021/07/15 20:30</div>
-      <div class="slot divide-slot"></div>
-    </div>
-  </div>
+  <MirrorDiffRow/>
 </div>
